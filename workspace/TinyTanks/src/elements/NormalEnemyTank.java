249349a391ba38c3a23d8edaf95667a0b24c371f
Jeremy Marchesani <jeremy12@iastate.edu>
2020-05-25 20:26:24 +0100

    added a pictures of tanks as well as an enemy tanks and user tank class

---
+++ workspace/TinyTanks/src/elements/NormalEnemyTank.java
@@ -0,0 +1,21 @@
+package elements;
+
+import javax.swing.ImageIcon;
+
+public class NormalEnemyTank extends Tank
+{
+	public NormalEnemyTank()
+	{
+		super.tankIcon = new ImageIcon("TankPics/userTank/UserTank.png");
+	}
+	
+	public void shoot()
+	{
+		
+	}
+	
+	public void move()
+	{
+		
+	}
+}
---
+++ workspace/TinyTanks/src/elements/UserTank.java
@@ -0,0 +1,21 @@
+package elements;
+
+import javax.swing.ImageIcon;
+
+public class UserTank extends Tank
+{
+	public UserTank()
+	{
+		super.tankIcon = new ImageIcon("TankPics/userTank/UserTank.png");
+	}
+	
+	public void shoot()
+	{
+		
+	}
+	
+	public void move()
+	{
+		
+	}
+}
